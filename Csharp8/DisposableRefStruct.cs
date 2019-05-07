4e24b6a3ca32f40868451c152eebe590ca5e5d60
Tobias Gärtner <tgaertner@recom.eu>
2019-05-07 16:52:43 +0200

    DisposableRefStruct

---
+++ Csharp8/DisposableRefStruct.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    class DisposableRefStruct
+    {
+        ref struct RefStruct
+        {
+            public int[] Data;
+
+            public void Dispose()
+            {
+                // release unmanaged resources
+            }
+        }
+
+        public void UseRefStruct()
+        {
+            using RefStruct refStruct = new RefStruct();
+
+            using (RefStruct refStruct2 = new RefStruct())
+            {
+
+            }
+        }
+    }
+}
